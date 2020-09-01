aef40797625a879d56c29cd358d8fa5fb99dabe7
Amaury Soto <amaurysotog@gmail.com>
2020-08-31 19:09:18 -0500

    Inicio Menú desplegable

---
+++ index.js
@@ -0,0 +1,47 @@
+const ipad = window.matchMedia("screen and (max-width: 767px)");
+const menu = document.getElementById("menuLeft");
+const burgerButton = document.getElementById("burger-menu");
+
+ipad.addListener(validation);
+
+function validation(evento) {
+  if (evento.matches) {
+    burgerButton.addEventListener("click", hideShow);
+  } else burgerButton.removeEventListener("click", hideShow);
+  console.log(evento.matches);
+}
+validation(ipad);
+
+function hideShow() {
+  if (menu.classList.contains("is-active")) {
+    menu.classList.remove("is-active");
+  } else {
+    menu.classList.add("is-active");
+  }
+}
+//CERRAR MENÚ//
+const close = document.getElementById("close");
+close.addEventListener("click", closeMenu);
+
+function closeMenu() {
+  menu.classList.remove("is-active");
+}
+
+/*  const menul = document.getElementById("menu-left");
+   menul.addEventListener("click", function () {
+     const menuDesple = document.getElementById("menu-desple");
+     if (menuDesple.style.display == "none") {
+       menuDesple.style.display = "block";
+       menuDesple.style.left = "0";
+     } else {
+       menuDesple.style.display = "none"
+
+     }
+
+   });
+
+   const botonCerrar = document.getElementById("cerrar");
+   botonCerrar.addEventListener("click", () => {
+     menuDesple.style.display = "block"
+   }
+   ) */
