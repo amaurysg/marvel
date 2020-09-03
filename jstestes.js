2b24c1c9a679d9bdec0d330eae46f2b9c3fe5578
Amaury Soto <amaurysotog@gmail.com>
2020-09-03 00:08:48 -0500

    Barras Menú automaticas

--- jstestes.js
+++ jstestes.js
@@ -1,5 +1,5 @@
 var slideIndex = 0;
-showSlides();
+window.addEventListener("load", showSlides);
 
 function showSlides() {
   var i;
@@ -13,6 +13,23 @@ function showSlides() {
   }
   slidesList[slideIndex - 1].style.opacity = "1";
   setTimeout(showSlides, 5000);
+}
+
+var menuIndex = 0;
+window.addEventListener("load", showMenuLoad);
+
+function showMenuLoad() {
+  var m;
+  var menuList = document.querySelectorAll(".menu-list");
+  for (m = 0; m < menuList.length; m++) {
+    menuList[m].style.borderBottom = "3px solid white";
+  }
+  menuIndex++;
+  if (menuIndex > menuList.length) {
+    menuIndex = 1;
+  }
+  menuList[menuIndex - 1].style.borderBottom = "3px solid #e62429";
+  setTimeout(showMenuLoad, 5000);
 }
 
 /* const boton = document.querySelector(".icon-search");
