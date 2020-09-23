47f6ed4edef27681b725f5a8aee898884baa3533
Amaury Soto <amaurysotog@gmail.com>
2020-09-23 15:15:31 -0500

    Cambio de logo con Scroll

--- jslogo.js
+++ jslogo.js
@@ -1,4 +1,20 @@
+/* 
+A manera de pruebas decídí hacer este codigo asignando manualmente las propiedas, tambien lo puedo hacer cambiando clases */
+var lastScrollTop = 0;
 var logo1 = document.getElementById("logo1mobile");
-logo1.addEventListener("onload", () => {
-  console.log("Hola Mundo");
+
+window.addEventListener("scroll", () => {
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrollTop > lastScrollTop) {
+    logo1.src = "./images/logo-marvel-M.png";
+    logo1.style.width = "52px";
+    logo1.style.position = "absolute";
+    logo1.style.left = "39px";
+  } else {
+    logo1.src = "./images/MarvelLogo.svg";
+    logo1.style.width = "130px";
+    logo1.style.position = "absolute";
+    logo1.style.left = "0";
+  }
+  lastScrollTop = scrollTop;
 });
