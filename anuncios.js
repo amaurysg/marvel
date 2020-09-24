5f8551f77a3627ec1842bcdf76326ee9c50c35f8
Amaury Soto <amaurysotog@gmail.com>
2020-09-24 14:50:02 -0500

    Mejoras hero principal, height -839px

--- anuncios.js
+++ anuncios.js
@@ -5,15 +5,27 @@ var contenido = document.createTextNode("Hola");
 //3- añadimos el nodo de texto al anuncio
 anuncio.appendChild(contenido);
 //4- Agregamos atributos
+// "class", nombre clase // OJOOO !!
 anuncio.setAttribute("align", "center");
 //5-Agrego anuncio al documento
 /* document.getElementById("anuncio1").appendChild(anuncio); */
 
 window.addEventListener("load", showAd);
+window.addEventListener("load", hiddenAd);
 
 function showAd() {
   setTimeout(() => {
     document.getElementById("anuncio1").appendChild(anuncio);
+
     console.log("debería mostrar");
-  }, 20000);
+  }, 2000);
 }
+function hiddenAd() {
+  setTimeout(() => {
+    document.getElementById("anuncio1").removeChild(anuncio);
+
+    console.log("debería ocultar");
+  }, 5000);
+}
+setInterval(showAd, 5000);
+setInterval(hiddenAd, 10000);
